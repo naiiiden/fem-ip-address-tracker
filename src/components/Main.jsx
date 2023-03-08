6b498011e482b84a2052653654bf599332378097
naiiiden <na4ko359@gmail.com>
2023-03-08 13:34:10 +0200

    style data-container

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,44 +3,41 @@ const Main = () => {
         <main>
 
 
-            <div className="input-data-container"
-                style={{
-                    "position": "absolute",
-                    "left": 0,
-                    "right": 0,
-                    "margin": "0 2rem",
-                    "top": "6.5rem",
-                    "display": "flex",
-                    "flexDirection": "column",
-                    "gap": "2rem",
-                }}
-            >
+            <div className="input-data-container">
                 <div className="input-submit-container">
                     <input type="text" />
                     <input type="submit" value=">"/>
                 </div>
-                <div style={{"textAlign": "center"}}>
-                    <div style={{"background": "green", "padding": ".5rem"}}>
-                        <div>IP ADDRESS</div>
-                        <div>192.212.174.101</div>
+                <div className="data-container">
+                    <div className="data-text">
+                        <p>IP Address</p>
+                        <p>192.212.174.101</p>
                     </div>
-                    <div style={{"background": "red", "padding": ".5rem"}}> 
-                        <div>LOCATION</div>
-                        <div>Brookyln, NY 10001</div>
+                    <div className="data-text"> 
+                        <p>Location</p>
+                        <p>Brooklyn, NY 10001</p>
                     </div>
-                    <div style={{"background": "hotpink", "padding": ".5rem"}}>
-                        <div>TIMEZONE</div>
-                        <div>UTC - 05:00</div>
+                    <div className="data-text">
+                        <p>Timezone</p>
+                        <p>UTC - 05:00</p>
                     </div>
-                    <div style={{"background": "yellow", "padding": ".5rem"}}>
-                        <div>ISP</div>
-                        <div>SpaceX Starlink</div>
+                    <div className="data-text">
+                        <p>ISP</p>
+                        <p>SpaceX Starlink</p>
                     </div>
                 </div>
             </div>
 
-
-            <h1>test</h1>
+            <h1>asd</h1>
+            <h1>useless</h1>
+            <h1>text</h1>
+            <h1>text</h1>
+            <h1>text</h1>
+            <h1>text</h1>
+            <h1>text</h1>
+            <h1>text</h1>
+            <h1>text</h1>
+            <h1>text</h1>
         </main>
     )
 }
