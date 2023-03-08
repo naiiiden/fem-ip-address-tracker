e1d6120e58c7a2bcbe57cd2053790fb74528e60b
naiiiden <na4ko359@gmail.com>
2023-03-08 06:04:15 +0200

    Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./style/general.css";
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <div className="App">
-      <p>hey</p>
+      <Header/>
     </div>
   )
 }
