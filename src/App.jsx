6146079a00e917e209b9aa1efefd3217439b1c8a
naiiiden <na4ko359@gmail.com>
2023-03-08 08:31:52 +0200

    position input & displayed data on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./style/general.css";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
 
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   )
 }
