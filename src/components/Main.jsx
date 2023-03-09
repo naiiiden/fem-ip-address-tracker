ab1ce986daa86894c6f6565aa2631880c441e21d
naiiiden <na4ko359@gmail.com>
2023-03-09 16:25:58 +0200

    test fetch

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,57 @@
+const api_key = import.meta.env.VITE_GEO_API_KEY;
+import { useState, useEffect } from "react";
+
 const Main = () => {
+    const [data, setData] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+
+    useEffect(() => {
+        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${api_key}&ipAddress=${inputValue}`)
+            .then((response) => response.json())
+            .then((data) => {setData(data); console.log(data)})
+            .catch((error) => console.log(error));
+    }, []);
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${api_key}&ipAddress=${inputValue}`)
+          .then((response) => response.json())
+          .then((data) => {
+            setData(data);
+            console.log(data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    };
+
     return (
         <main>
             <div className="input-data-container">
-                <form className="input-submit-container">
-                    <input type="text" placeholder="Search for any IP address or domain" aria-label="Search for any IP address or domain"/>
+                <form className="input-submit-container" onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Search for any IP address or domain" aria-label="Search for any IP address or domain" value={inputValue} onChange={handleInputChange}/>
                     <input type="submit" value="Send request"/>
                 </form>
                 <div className="data-container">
                     <div className="data-text">
                         <p>IP Address</p>
-                        <p>192.212.174.101</p>
+                        {data?.ip}
                     </div>
                     <div className="data-text"> 
                         <p>Location</p>
-                        <p>Brooklyn, NY 10001</p>
+                        {`${data?.location?.city}, ${data?.location?.region} ${data?.location?.postalCode}`}
                     </div>
                     <div className="data-text">
                         <p>Timezone</p>
-                        <p>UTC - 05:00</p>
+                        {data?.location?.timezone}
                     </div>
                     <div className="data-text">
                         <p>ISP</p>
-                        <p>SpaceX Starlink</p>
+                        {data?.isp}
                     </div>
                 </div>
             </div>
