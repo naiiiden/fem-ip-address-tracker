c092669e2b1e272caef5d0ba27142829909e9fad
naiiiden <na4ko359@gmail.com>
2023-06-02 16:57:44 +0300

    reposition zoom control

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 const api_key = import.meta.env.VITE_GEO_API_KEY;
 import { useState, useEffect } from "react";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, ZoomControl } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 
 const newicon = new L.icon({
@@ -63,19 +63,20 @@ const Main = () => {
                 </div>
             </div>
 
-            {/* <div id="map"> */}
-                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                    <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} zoomControl={false}>
+                <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                    <ZoomControl position="bottomright">
+
+                    </ZoomControl>
                     <Marker position={[51.505, -0.09]} icon={newicon}>
                         <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                         </Popup>
                     </Marker>
-                </MapContainer>
-            {/* </div> */}
+            </MapContainer>
         </main>
     )
 }
